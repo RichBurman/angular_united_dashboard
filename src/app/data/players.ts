462dc5d0cab2c58a67b50de6ab5832fad2ba28c5
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-24 13:05:09 +0100

    feat: Add player card component with template, styles, and tests

---
+++ src/app/data/players.ts
@@ -0,0 +1,20 @@
+export const players = [
+  {
+    id: 1,
+    name: 'Bruno Fernandes',
+    position: 'Midfielder',
+    number: 8
+  },
+  {
+    id: 2,
+    name: 'Kobbie Mainoo',
+    position: 'Midfielder',
+    number: 37
+  },
+  {
+    id: 3,
+    name: 'Matheus Cunha',
+    position: 'Forward',
+    number: 10
+  }
+];
