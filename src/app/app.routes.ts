ffab9ff577e11528008f7c2ff118f1a51cd77d14
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-24 12:16:53 +0100

    feat: Add NotFound component with routing, template, styles, and tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
   {
     path: 'home',
     component: Home,
   },
+  {
+    path: '**',
+    component: NotFound
+  }
 ];
