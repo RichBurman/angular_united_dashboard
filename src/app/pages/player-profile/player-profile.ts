11461f06ea21d243f5116950cc2e9b091c6d2945
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-27 13:18:39 +0100

    feat: Add back navigation button and implement goBack method in player profile component

--- src/app/pages/player-profile/player-profile.ts
+++ src/app/pages/player-profile/player-profile.ts
@@ -1,7 +1,8 @@
-import id from '@angular/common/locales/extra/id';
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlayerService } from '../../services/player';
+
 
 @Component({
   selector: 'app-player-profile',
@@ -11,10 +12,15 @@ import { PlayerService } from '../../services/player';
 })
 export class PlayerProfile {
   private route = inject(ActivatedRoute);
+  private location = inject(Location);
 
   id = this.route.snapshot.paramMap.get('id');
 
   private playerService = inject(PlayerService);
 
   player = this.playerService.getPlayerById(Number(this.id));
+
+  goBack() {
+    this.location.back();
+  }
 }
