a9547ef3b6f185c4904321cfd230b8c658394cfa
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-07-27 13:29:05 +0100

    feat: Implement favorite toggle functionality in player profile component

--- src/app/pages/player-profile/player-profile.ts
+++ src/app/pages/player-profile/player-profile.ts
@@ -1,8 +1,7 @@
 import { Location } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlayerService } from '../../services/player';
-
 
 @Component({
   selector: 'app-player-profile',
@@ -14,13 +13,32 @@ export class PlayerProfile {
   private route = inject(ActivatedRoute);
   private location = inject(Location);
 
+  constructor() {
+    effect(() => {
+      localStorage.setItem(
+        `player-${this.player?.id}-favorite`,
+        JSON.stringify(this.favorite()),
+      );
+    });
+  }
+
   id = this.route.snapshot.paramMap.get('id');
 
   private playerService = inject(PlayerService);
 
   player = this.playerService.getPlayerById(Number(this.id));
 
+  favorite = signal(
+    JSON.parse(
+      localStorage.getItem(`player-${this.player?.id}-favorite`) ?? 'false',
+    ),
+  );
+
   goBack() {
     this.location.back();
+  }
+
+  toggleFavorite() {
+    this.favorite.update((value) => !value);
   }
 }
