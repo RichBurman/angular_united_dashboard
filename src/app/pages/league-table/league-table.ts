aabb7d4c71579c49ae6d45ccb05eaf618b27df53
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-08-04 10:22:05 +0100

    feat: Implement league table display with responsive design and data binding

--- src/app/pages/league-table/league-table.ts
+++ src/app/pages/league-table/league-table.ts
@@ -1,19 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { LeagueService } from '../../services/league';
+import { LeagueTableResponse } from '../../models/league-table';
 
 @Component({
   selector: 'app-league-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './league-table.html',
   styleUrl: './league-table.css',
 })
 export class LeagueTable {
+  private leagueService = inject(LeagueService);
 
-    private leagueService = inject(LeagueService);
+  standings: LeagueTableResponse['standings'] = [];
 
-    constructor() {
-      this.leagueService.getLeagueTable().subscribe((data) => {
-        console.log(data);
-      })
-    }
+  constructor() {
+    this.leagueService.getLeagueTable().subscribe((data) => {
+      this.standings = data.standings;
+    });
+  }
 }
